da05550edcfcd3f16a6af3a33e83128b7bdd76f6
Piyush Ravi <piyush.ravi_ug19@ashoka.edu.in>
2017-05-11 18:37:40 +0530

    Update LinearEquationsSolver.py

--- LinearEquationsSolver.py
+++ LinearEquationsSolver.py
@@ -1,5 +1,5 @@
 #CS101 Project
-#Linear equations solver using Gaussian elimination for equations with integer coefficients. 
+#Linear equations solver using Gaussian elimination for equations with integer coefficients.
 #Python 3.6
 
 import sys, os
@@ -7,7 +7,7 @@ import sys, os
 '''
 The standard input textfile will contain an integer T in the first line, denoting the number of system of linear euations to be solved.
 The first line of each test case will contain two numbers num_var and num_eq denoting the number of variables and number of equations.
-The next num_eq lines will num_var+1 integers, denoting a equation of the form (a*x_1) + (b*x_2)=c, with a, b and c stored as values. 
+The next num_eq lines will num_var+1 integers, denoting a equation of the form (a*x_1) + (b*x_2)=c, with a, b and c stored as values.
 '''
 
 def GCD(num_1, num_2):
@@ -15,7 +15,7 @@ def GCD(num_1, num_2):
         return num_2
     else:
         return GCD(num_2%num_1, num_1)
-    
+
 
 #Class for rational numbers
 class irreducibleFractions:
@@ -31,42 +31,42 @@ class irreducibleFractions:
                 num_1=-num_1
             if num_2<0:
                 num_2=-num_2
-                    
+
             g=GCD(num_1, num_2)
             if not sign:
                 self.num=-(num_1//g)
                 self.deno=num_2//g
-            
+
             else:
                 self.num=num_1//g
                 self.deno=num_2//g
-    
+
     def __init__(self, num_1, num_2):
         self.num=num_1
         self.deno=num_2
         self.update(num_1, num_2)
-            
-                
-    
-    
+
+
+
+
     def __str__(self):
         return str(self.num)+'/'+str(self.deno)
-    
+
     def getNum(self):
         return self.num
-    
+
     def getDeno(self):
         return self.deno
-    
 
-        
-           
+
+
+
 
 def equationSolver(sys_Eq, rows, cols):
     #Converting the augmented matrix sys_Eq to row reduced echleon form
     row=rows
     col=cols+1 #Considering the augmented matrix.
-    
+
     #Row reducing to echelon form using Gaussian Elimination
     for i in range(row):
         for j in range(col):
@@ -78,39 +78,40 @@ def equationSolver(sys_Eq, rows, cols):
                         for z in range(j, col):
                             sys_Eq[y][z]=coeff2*sys_Eq[y][z]-coeff1*sys_Eq[i][z]
                 break
-                    
-    fracObjs=[]            
+
+    fracObjs=[]
     for i in range(row):
         temp=[]
         for j in range(col):
             temp.append([])
         fracObjs.append(temp)
-    
+
     num_2=0
     for i in range(row):
         for j in range(col):
             if sys_Eq[i][j]!=0:
                 num_2=sys_Eq[i][j]
+                break
         for j in range(col):
             fracObjs[i][j]=irreducibleFractions(sys_Eq[i][j], num_2)
-    
-    
-    
-    #Checking for consistency        
+
+
+
+    #Checking for consistency
     for i in range(row):
         for j in range(col):
             if sys_Eq[i][j]!=0 and j==cols:
                 return [False]
             elif sys_Eq[i][j]!=0 and j!=cols:
                 break
-                
-                
+
+
     result=[]
     for x in range(cols):
         result.append('')
-    
 
-        
+
+
     #Finding the values of num_var variables.
     for i in range(row):
         for j in range(cols):
@@ -119,21 +120,22 @@ def equationSolver(sys_Eq, rows, cols):
                     if fracObjs[i][y].__str__()!='0/1':
                         if result[j]!='':
                             result[j]+='+'
+                        fracObjs[i][y].update(-(fracObjs[i][y].getNum()), fracObjs[i][y].getDeno())
                         result[j]+='('+fracObjs[i][y].__str__()+' * x_'+str(y+1)+')'
                 if fracObjs[i][cols].__str__()!='0/1':
                     if result[j]!='':
                         result[j]+='+'
                     result[j]+='('+fracObjs[i][cols].__str__()+')'
                 break
-    
+
     return [True]+result
-        
-                
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
 filename=''
 try:
     filename=sys.argv[1]
@@ -141,7 +143,7 @@ except:
     while filename=='' or not os.path.exists(filename):
         filename=input("Enter a valid input text filename:")
 
-        
+
 '''
 The output file will be named as Result of [input_filename].
 The output file for every test case will num_var lines if the system is consitent,
@@ -169,10 +171,10 @@ for x in range(N):
     for y in range(num_eq):
         sys_Eq.append(list(map(int, inp[ctr].split())))
         ctr+=1
-    
-    result=equationSolver(sys_Eq, num_eq, num_var)            
+
+    result=equationSolver(sys_Eq, num_eq, num_var)
     wfile.write("Solution to Case #"+str(x+1)+": \n")
-    
+
     if result[0]==False:
         wfile.write("The given system of equations is inconsistent.\n")
     else:
@@ -181,13 +183,9 @@ for x in range(N):
                 wfile.write('x_'+str(y+1)+ ' is free.\n')
             else:
                 wfile.write('x_'+str(y+1)+' = '+result[y+1]+'\n')
-    
+
     print('Completed Test Case '+str(x+1)+'!!\n')
-    
-        
-    
-wfile.close()        
-    
-        
-    
-    
+
+
+
+wfile.close()
